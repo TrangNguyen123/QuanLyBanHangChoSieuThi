6100e7036427a792a60e99b29ed6ab9d5a5d512e
huelekim <hue1996mta@gmail.com>
2017-04-18 08:56:20 +0700

    Bảng nhân viên
    Bảng nhân viên

--- QUANLY_BHST/CONTROL/NHACUNGCAP_C.cs
+++ QUANLY_BHST/CONTROL/NHACUNGCAP_C.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MODEL.ENTITIES;
-using MODEL.FUNCTION;
+using MODAL.ENNITES;
+using MODAL.FUNSIONS;
 using System.Data;
 using System.Data.SqlClient;
 namespace CONTROL
